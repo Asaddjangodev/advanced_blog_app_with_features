ed4c2423afb7599fe3ca42895d3d2ed3c16b8dc3
Asadbek <hoshimovaustin0550@gmail.com>
2025-08-04 12:17:12 +0900

    craete Post model and configure admin panel settings

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+#from django.utils.text import slugify
+from autoslug import AutoSlugField
+
 # Create your models here.
 class Post(models.Model):
 
@@ -10,11 +13,18 @@ class Post(models.Model):
     )
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
-    publish_date = models.DateTimeField(default=timezone.now)
+    publish = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     content = models.TextField()
     status = models.CharField(max_length=10, choices=options, default='draft')
 
+    # def save(self, *args, **kwargs):
+    #     if not self.slug:
+    #         self.slug = slugify(self.title)
+    #     super().save(*args, **kwargs)
+
+    class Meta:
+        ordering = ['-publish']
 
     def __str__(self):
         return self.title
