91dae57c99b33d14950546b1fb16a4af51e99eec
Asadbek <hoshimovaustin0550@gmail.com>
2025-08-06 15:07:50 +0900

    Modified admin panel to show MPTTModel Comments

--- blog/admin.py
+++ blog/admin.py
@@ -1,22 +1,17 @@
 from django.contrib import admin
 from .models import Post, Comments, Category
+from mptt.admin import MPTTModelAdmin
 # Register your models here.
 
 
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('title', 'status' ,'slug', 'author')
-
+    list_display = ('title', 'id' ,'status' ,'slug', 'author')
 admin.site.register(Post, AuthorAdmin)
 
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ('post', 'name', 'email', 'publish', 'status')
-    list_filter = ('status', 'publish')
-    search_fields = ('name', 'email', 'content')
 
-
-admin.site.register(Comments,CommentAdmin)
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name',)
-
 admin.site.register(Category, CategoryAdmin)
+
+admin.site.register(Comments, MPTTModelAdmin)
