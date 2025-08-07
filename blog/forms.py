ae0c191c54f998a8b714ab0a45acec3da5690a82
Asadbek <hoshimovaustin0550@gmail.com>
2025-08-07 13:42:14 +0900

    Add TreeNodeChoiceField for parent comment selection with MPTT support - Pragmmatic parent assignment while keeping UI simple - Support for both root-level and reply comments - Clean integration with MPTT tree structure

--- blog/forms.py
+++ blog/forms.py
@@ -1,10 +1,24 @@
 from django import forms
 from .models import Comments
+from mptt.forms import TreeNodeChoiceField
+
+
 
 class NewCommentForm(forms.ModelForm):
+    parent = TreeNodeChoiceField(queryset=Comments.objects.all())
+    # будет требовать выбрать родителя для комментария
+
+    def __init__(self, *args, **kwargs): # даёт доступ не выбрать родителя
+        super().__init__(*args, **kwargs)
+        self.fields['parent'].widget.attrs.update(
+            {'class': 'd-none'}
+        )
+        self.fields['parent'].label = ''
+        self.fields['parent'].required = False
+
     class Meta:
         model = Comments
-        fields = ('name', 'email', 'content')
+        fields = ('name','parent','email', 'content')
         widgets = {
             "name": forms.TextInput(attrs={'class': 'col-sm-12'}),
             "email": forms.TextInput(attrs={'class': 'col-sm-12'}),
