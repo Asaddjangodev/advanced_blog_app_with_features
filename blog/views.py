693807122b9c7048e9ee5e243863058fa9cb9db9
Asadbek <hoshimovaustin0550@gmail.com>
2025-08-09 18:52:48 +0900

    add post_search functionality

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Category, Comments
-from .forms import NewCommentForm
+from .forms import NewCommentForm, PostSearchForm
 from django.http import HttpResponseRedirect
 from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -37,7 +37,7 @@ def post_single(request, post):
     else:
         comment_form = NewCommentForm()
     return render(request, "single.html", {'post': post, "comments": comments, "comment_form": comment_form,
-                                         "page": page, "paginator": paginator})
+                                         "page": page, "paginator": paginator, "allcomments": allcomments})
 
     #return render(request, 'single.html', {'post': post})
 
@@ -59,3 +59,8 @@ def category_list(request):
         'category_list': category_list,
     }
     return context
+
+def post_search(request):
+    form = PostSearchForm
+    q = ''
+    return render(request, 'search.html', {'form': form, 'q': q})
