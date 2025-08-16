991214a44e0b2d004c10ed1a476d4931f186d83c
Asadbek <hoshimovaustin0550@gmail.com>
2025-08-16 20:29:17 +0900

    add authentication token and user registration/activation functions

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.http import HttpResponse
+from .forms import RegistrationForm
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from django.utils.encoding import force_bytes, force_str
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -9,18 +17,50 @@ from django.contrib.auth import login
 def profile_view(request):
     return render(request, 'accounts/profile.html')
 
-
-def register_view(request):
+# Регистрирует нового пользователя, деактивирует аккаунт и отправляет email с токеном активации
+def account_register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('account:profile')
+        registerForm = RegistrationForm(request.POST)
+        if registerForm.is_valid():
+            user = registerForm.save(commit=False)
+            user.email = registerForm.cleaned_data['email']
+            user.set_password(registerForm.cleaned_data['password'])
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            subject = 'Activate your Account'
+            message = render_to_string(
+                'registration/account_activation_email.html',{
+                    'user':user,
+                    'domain':current_site.domain,
+                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                    'token': account_activation_token.make_token(user),
+                }
+            )
+            user.email_user(subject=subject, message=message)
+            return HttpResponse('registered successfully and activatoin sent')
     else:
-        form = UserCreationForm()
-    return render(request, 'accounts/register.html', {'form': form})
+        registerForm = RegistrationForm()
+    return render(request, 'registration/register.html', {'form': registerForm})
 
 
 def edit(request):
     return render(request, 'accounts/edit.html')
+
+# Активирует аккаунт пользователя по токену из email, авторизует при успехе или возвращает ошибку
+def activate(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode((uidb64)))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return redirect('login')
+    else:
+        return render(request, 'registration/activation_invalid.html')
+
+
+
